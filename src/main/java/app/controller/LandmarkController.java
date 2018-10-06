0653148c2eb155237d2172780b10496eb1667668
Daniel <daniel.zhang@berkeley.edu>
2018-10-05 22:49:04 -0700

    rename locationcontroller to landmark controller

--- src/main/java/app/controller/LandmarkController.java
+++ src/main/java/app/controller/LandmarkController.java
@@ -4,6 +4,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
-public class LocationController {
+public class LandmarkController {
 
 }
